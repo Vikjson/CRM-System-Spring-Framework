9e3afb28eca64afbfef3cb7bafad2fa926e8cbea
Viktoria Johansson <viktoria.k.johansson@gmail.com>
2026-04-20 19:32:37 +0200

    Complete metods in CustomerManagementMockImp, DiaryManagementServiceMockImp and added bean customerManagement in application.xml. Added customer list in SimpleClient

--- src/main/java/se/yrgo/client/SimpleClient.java
+++ src/main/java/se/yrgo/client/SimpleClient.java
@@ -1,10 +1,26 @@
 package se.yrgo.client;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import se.yrgo.domain.Customer;
+import se.yrgo.services.customers.CustomerManagementService;
 
+import java.util.List;
 
 public class SimpleClient {
 
     public static void main(String[] args) {
+        ClassPathXmlApplicationContext container =
+                new ClassPathXmlApplicationContext("application.xml");
+        CustomerManagementService customerManagement =
+                container.getBean("customerManagement", CustomerManagementService.class);
+
+        List<Customer> customers = customerManagement.getAllCustomers();
+
+        for (Customer customer : customers) {
+            System.out.println(customer);
+        }
+
+        container.close();
 
     }
 }
--- src/main/java/se/yrgo/services/customers/CustomerManagementMockImpl.java
+++ src/main/java/se/yrgo/services/customers/CustomerManagementMockImpl.java
@@ -1,5 +1,6 @@
 package se.yrgo.services.customers;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
@@ -7,63 +8,67 @@ import se.yrgo.domain.Call;
 import se.yrgo.domain.Customer;
 
 public class CustomerManagementMockImpl implements CustomerManagementService {
-	private HashMap<String,Customer> customerMap;
+    private HashMap<String, Customer> customerMap;
 
-	public CustomerManagementMockImpl() {
-		customerMap = new HashMap<String,Customer>();
-		customerMap.put("OB74", new Customer("OB74" ,"Fargo Ltd", "some notes"));
-		customerMap.put("NV10", new Customer("NV10" ,"North Ltd", "some other notes"));
-		customerMap.put("RM210", new Customer("RM210" ,"River Ltd", "some more notes"));
-	}
-
-
-	@Override
-	public void newCustomer(Customer newCustomer) {
-
-	}
-
-	@Override
-	public void updateCustomer(Customer changedCustomer) {
+    public CustomerManagementMockImpl() {
+        customerMap = new HashMap<String, Customer>();
+        customerMap.put("OB74", new Customer("OB74", "Fargo Ltd", "some notes"));
+        customerMap.put("NV10", new Customer("NV10", "North Ltd", "some other notes"));
+        customerMap.put("RM210", new Customer("RM210", "River Ltd", "some more notes"));
+    }
 
 
-	}
+    @Override
+    public void newCustomer(Customer newCustomer) {
+        customerMap.put(newCustomer.getCustomerId(), newCustomer);
+    }
 
-	@Override
-	public void deleteCustomer(Customer oldCustomer) {
-		// TODO Auto-generated method stub
+    @Override
+    public void updateCustomer(Customer changedCustomer) {
+        customerMap.put(changedCustomer.getCustomerId(), changedCustomer);
+    }
 
-	}
+    @Override
+    public void deleteCustomer(Customer oldCustomer) {
+        customerMap.remove(oldCustomer.getCustomerId());
+    }
 
-	@Override
-	public Customer findCustomerById(String customerId) throws CustomerNotFoundException {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public Customer findCustomerById(String customerId) throws CustomerNotFoundException {
+        Customer foundCustomer = customerMap.get(customerId);
 
-	@Override
-	public List<Customer> findCustomersByName(String name) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+        if (foundCustomer == null) {
+            throw new CustomerNotFoundException();
+        }
+        return foundCustomer;
+    }
 
-	@Override
-	public List<Customer> getAllCustomers() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public List<Customer> findCustomersByName(String name) {
+        List<Customer> foundCustomers = new ArrayList<>();
 
-	@Override
-	public Customer getFullCustomerDetail(String customerId) throws CustomerNotFoundException {
-		// TODO Auto-generated method stub
-		return null;
-	}
+        for (Customer customer : customerMap.values()) {
+            if (customer.getCompanyName().toLowerCase().contains(name.toLowerCase())) {
+                foundCustomers.add(customer);
+            }
+        }
+        return foundCustomers;
+    }
 
-	@Override
-	public void recordCall(String customerId, Call callDetails) throws CustomerNotFoundException {
-		//First find the customer
+    @Override
+    public List<Customer> getAllCustomers() {
+        return new ArrayList<Customer>(customerMap.values());
+    }
 
-		//Call the addCall on the customer
+    @Override
+    public Customer getFullCustomerDetail(String customerId) throws CustomerNotFoundException {
+        return findCustomerById(customerId);
+    }
 
-	}
+    @Override
+    public void recordCall(String customerId, Call callDetails) throws CustomerNotFoundException {
+        Customer foundCustomer = findCustomerById(customerId);
+        foundCustomer.addCall(callDetails);
+    }
 
 }
